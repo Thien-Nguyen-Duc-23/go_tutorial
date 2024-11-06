da03ba2c23d7a294a8041220c6bf853eae770f00
COC.ThienND <thiennd@concrete-corp.com>
2024-11-06 16:57:52 +0700

    Tour of go chapter Pointer receivers

--- tour_of_go/hello.go
+++ tour_of_go/hello.go
@@ -6,6 +6,7 @@ import (
 	"math/cmplx"
 	"math/rand"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -71,7 +72,11 @@ func pow(x, n, lim float64) float64 {
 
 // Exercise: Loops and Functions
 
-// Switch
+// Structs
+type Vertex struct {
+	X int
+	Y int
+}
 
 func main() {
 	fmt.Printf("Hello, Thien")
@@ -161,4 +166,378 @@ func main() {
 		// plan9, windows...
 		fmt.Printf("%s.\n", os)
 	}
+
+	fmt.Println("----------------- Switch evaluation order -----------------")
+
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+
+	fmt.Println("----------------- Switch with no condition -----------------")
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+
+	fmt.Println("----------------- Defer -----------------")
+	defer fmt.Println("world")
+
+	fmt.Println("hello")
+
+	fmt.Println("----------------- Stacking defers -----------------")
+	fmt.Println("counting")
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("done")
+
+	fmt.Println("----------------- Pointers -----------------")
+	i, j := 42, 2701
+
+	p := &i
+	fmt.Println(*p)
+	*p = 21
+	fmt.Println(i)
+
+	p = &j
+	*p = *p / 37
+	fmt.Println(j)
+
+	fmt.Println("----------------- Structs -----------------")
+	fmt.Println(Vertex{1, 2})
+
+	fmt.Println("----------------- Struct Fields -----------------")
+	vv := Vertex{1, 2}
+	fmt.Println(vv.X)
+	vv.X = 4
+	fmt.Println(vv.X)
+
+	fmt.Println("----------------- Pointers to structs -----------------")
+	vvv := Vertex{1, 2}
+	ppp := &vvv
+	ppp.X = 1e9
+	fmt.Println(vvv)
+
+	fmt.Println("----------------- Struct Literals -----------------")
+	fmt.Println(v1, p1, v2, v3)
+
+	fmt.Println("----------------- Arrays -----------------")
+	var a1 [2]string
+	a1[0] = "Hello"
+	a1[1] = "World"
+	fmt.Println(a1[0], a1[1])
+	fmt.Println(a1)
+
+	fmt.Println("----------------- Slices -----------------")
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+
+	fmt.Println("----------------- Slices are like references to arrays -----------------")
+	names1 := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names1)
+	a2 := names1[1:2]
+	b1 := names1[1:3]
+	fmt.Println(a2, b1)
+
+	b1[0] = "XXX"
+	fmt.Println(a1, b1)
+	fmt.Println(names1)
+
+	fmt.Println("----------------- Slice literals -----------------")
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	s := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(s)
+
+	fmt.Println("----------------- Slice defaults -----------------")
+	s1 := []int{2, 3, 5, 7, 11, 13}
+	s1 = s1[1:4]
+	fmt.Println(s1)
+
+	s1 = s1[:4]
+	fmt.Println(s1)
+
+	s1 = s1[1:]
+	fmt.Println(s1)
+
+	fmt.Println("----------------- Slice length and capacity -----------------")
+	sliceLengthAndCapacity()
+
+	fmt.Println("----------------- Creating a slice with make -----------------")
+	createSliceWithMake()
+
+	fmt.Println("----------------- Slices of slices	-----------------")
+	sliceOfSlice()
+
+	fmt.Println("----------------- Appending to a slice -----------------")
+	appendingToSlice()
+
+	fmt.Println("----------------- Range -----------------")
+	rangePow()
+
+	fmt.Println("----------------- Range continued -----------------")
+	rangeContinued()
+
+	fmt.Println("----------------- Maps -----------------")
+	mapsTour()
+
+	fmt.Println("----------------- Map Literals -----------------")
+	mapLiterals()
+
+	fmt.Println("----------------- Mutating Maps -----------------")
+	mutatingMaps()
+
+	fmt.Println("----------------- Function closures -----------------")
+	fucntionClouseres()
+
+	fmt.Println("----------------- Method tour -----------------")
+	mothodTour()
+	fmt.Println("----------------- End method tour -----------------")
+
+	fmt.Println("----------------- Methods continued -----------------")
+	fx := MyFloat(-math.Sqrt2)
+	fmt.Println(fx.Abs())
+	fmt.Println("----------------- Methods continued -----------------")
+
+	fmt.Println("----------------- Pointer receivers -----------------")
+	// fx := MyFloat(-math.Sqrt2)
+	// fmt.Println(fx.Abs())
+	fmt.Println("----------------- Pointer receivers -----------------")
+
+}
+
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p1 = &Vertex{1, 2}
+)
+
+// ----------------- Slice length and capacity -----------------
+func sliceLengthAndCapacity() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
+
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[2:]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+// ----------------- Creating a slice with make -----------------
+
+func printCreateSlice(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
+
+// Creating a slice with make
+func createSliceWithMake() {
+	a := make([]int, 5)
+	printCreateSlice("a", a)
+
+	b := make([]int, 0, 5)
+	printCreateSlice("b", b)
+
+	c := b[:2]
+	printCreateSlice("c", c)
+
+	d := c[2:5]
+	printCreateSlice("d", d)
+}
+
+// Slices of slices
+func sliceOfSlice() {
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}
+
+// Appending to a slice
+func appendingToSlice() {
+	var s []int
+	printSlice(s)
+
+	// append works on nil slices.
+	s = append(s, 0)
+	printSlice(s)
+
+	// The slice grows as needed.
+	s = append(s, 1)
+	printSlice(s)
+
+	// We can add more than one element at a time.
+	s = append(s, 2, 3, 4)
+	printSlice(s)
+}
+
+// Range
+func rangePow() {
+	var pow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+	for i, v := range pow {
+		fmt.Printf("i = %d \n", i)
+
+		fmt.Printf("v = %d \n", v)
+
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+}
+
+func rangeContinued() {
+	pow := make([]int, 10)
+	for i := range pow {
+		pow[i] = 1 << uint(i)
+	}
+
+	for _, value := range pow {
+		fmt.Printf("%d\n", value)
+	}
+}
+
+type VertexMaps struct {
+	Lat, Long float64
+}
+
+// maps
+func mapsTour() {
+	var m map[string]VertexMaps
+	m = make(map[string]VertexMaps)
+	m["Bell Labs"] = VertexMaps{
+		40.68433, -74.39967,
+	}
+	fmt.Println(m["Bell Labs"])
+}
+
+// map literals
+func mapLiterals() {
+	var m = map[string]VertexMaps{
+		"Bell Labs": VertexMaps{
+			40.68433, -74.39967,
+		},
+		"Google": VertexMaps{
+			37.42202, -122.08408,
+		},
+	}
+
+	fmt.Println(m)
+}
+
+// Mutating Maps
+func mutatingMaps() {
+	m := make(map[string]int)
+
+	m["Answer"] = 42
+	fmt.Println("The value:", m["Answer"])
+
+	m["Answer"] = 48
+	fmt.Println("The value:", m["Answer"])
+
+	delete(m, "Answer")
+	fmt.Println("The value:", m["Answer"])
+
+	v, ok := m["Answer"]
+	fmt.Println("The value:", v, "Present?", ok)
+}
+
+// Function closures
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func fucntionClouseres() {
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
+
+// Methods
+type VertexMethod struct {
+	X, Y float64
+}
+
+func (v VertexMethod) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func mothodTour() {
+	v := VertexMethod{3, 4}
+	fmt.Println(v.Abs())
+}
+
+// methods continued
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+
+	return float64(f)
 }
